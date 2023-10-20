00c0652c4f563d589768d2c1231badf66e588c55
DevSon0812 <97348196+DevSon0812@users.noreply.github.com>
2023-10-21 00:29:48 +0700

    update page-cart

--- src/containers/cart/CartProductList.tsx
+++ src/containers/cart/CartProductList.tsx
@@ -1,13 +1,43 @@
+'use client';
 import { ROUTER } from '@/utils/routes/routes';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
 import { MdKeyboardArrowDown, TbHeart } from '../../compound/icons/index';
+
 const CartProductList = () => {
+	// handle Quantity shopping cart
+	const [showQuantity, setShowQuantity] = useState<boolean>(false);
+	const [qty, setQty] = useState(1);
+	const dropdownRef = useRef<HTMLUListElement>(null);
+
+	const selectQuantity = (selectedQty: number) => {
+		setQty(selectedQty);
+		setShowQuantity(false);
+	};
+
+	const closeDropdown = (event: MouseEvent) => {
+		if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+			setShowQuantity(false);
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener('mousedown', closeDropdown);
+		return () => {
+			document.removeEventListener('mousedown', closeDropdown);
+		};
+	}, []);
+
+	const toggleDropdown = () => {
+		setShowQuantity(!showQuantity);
+	};
+
 	return (
 		<div className="bag-cart-box">
 			<div className="shopping-cart-title-box">
 				<h4 className="title _text-uppercase">shopping cart</h4>
-				<p className="qty">1 item</p>
+				<div className="qty">1 item</div>
 			</div>
 			<div className="shopping-cart-lists">
 				<h4 className="title">Ship</h4>
@@ -39,9 +69,26 @@ const CartProductList = () => {
 								<button
 									className="qty"
 									type="button"
+									onClick={toggleDropdown}
 								>
-									Qty: 1 <MdKeyboardArrowDown />
+									Qty: {qty} <MdKeyboardArrowDown />
 								</button>
+								{showQuantity && (
+									<ul
+										className="dropdown-inner"
+										ref={dropdownRef}
+									>
+										{[1, 2, 3, 4, 5, 6].map((value) => (
+											<li
+												className="item"
+												key={value}
+												onClick={() => selectQuantity(value)}
+											>
+												{value}
+											</li>
+										))}
+									</ul>
+								)}
 							</div>
 						</div>
 						<div className="item-total">
